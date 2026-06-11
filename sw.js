f594d479c6c0ab94715a468a2c528913f9ce9f32
nonaryworks <kawamika25@gmail.com>
2026-06-12 08:18:55 +0900

    SW: ネットワーク優先に変更・clients.claim追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'school-planner-v3'
+const CACHE = 'school-planner-v4'
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png']
 
 self.addEventListener('install', e => {
@@ -10,10 +10,18 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ))
+  self.clients.claim()
 })
 
 self.addEventListener('fetch', e => {
+  // ネットワーク優先・失敗時にキャッシュ
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   )
 })
