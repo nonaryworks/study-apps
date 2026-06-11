a96a90b3a07db2d5f6deece238587ad803e4182c
nonaryworks <kawamika25@gmail.com>
2026-06-12 08:16:19 +0900

    キャッシュバージョン更新 v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'school-planner-v1'
-const FILES = ['./index.html', './manifest.json']
+const CACHE = 'school-planner-v3'
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png']
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
